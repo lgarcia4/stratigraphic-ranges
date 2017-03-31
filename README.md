7732f0bade1fcf4bc91b615ff8157038ce270a2f
Gavryushkina  Alexandra <gavryusa@bs-mbpr192.d.ethz.ch>
2017-03-31 14:53:29 +0200

    update the name

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# chronospecies
+# stratigraphic-ranges
 A package to implement operators and tree likelihood function for trees with several samples assigned to the same species.  
--- operators/ChrWilsonBalding.java
+++ operators/ChrWilsonBalding.java
@@ -2,14 +2,14 @@ package operators;
 
 import beast.core.Input;
 import beast.evolution.operators.TreeOperator;
-import chronospecies.ChronospeciesSet;
+import sranges.StratigraphicRangeSet;
 
 /**
  * Created by gavryusa on 19/12/16.
  */
 public class ChrWilsonBalding extends TreeOperator {
 
-    public Input<ChronospeciesSet> chronospeciesInput = new Input<ChronospeciesSet>("chronospesies", "all chronospesies defined by the first and last occurrences");
+    public Input<StratigraphicRangeSet> chronospeciesInput = new Input<StratigraphicRangeSet>("chronospesies", "all chronospesies defined by the first and last occurrences");
 
 
     @Override
